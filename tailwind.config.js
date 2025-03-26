ddb603772e1feb3e2e8fd278103efa3f94d4d288
KewneWhimsy <josshanel@gmail.com>
2025-03-26 01:10:42 +0100

    styles tailwind +

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx  }"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        // Palette pour thème sombre global
-        primary: "#1a1a1a",
-        secondary: "#2a2a2a",
-        accent: "#3a773a", // Couleur verte écologique
-        text: "#e0e0e0",
-        "text-muted": "#a0a0a0",
-      },
+      // Autres personnalisations si nécessaire (spacing, fontSizes, etc.)
     },
   },
   plugins: [],
