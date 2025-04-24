9d6d70c6284704f58af15ebcf108d98133a34de3
KewneWhimsy <josshanel@gmail.com>
2025-04-24 09:55:42 +0200

    devis vitrine standard

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "always",
-  site: 'https://votredomaine.com', // Remplacez par votre domaine réel
+  site: 'https://jkw.ovh',
   vite: {
     plugins: [tailwindcss()]
   },
