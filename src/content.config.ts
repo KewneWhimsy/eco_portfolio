6d5f68302c15e0e4b18618fa56933d55684abdcf
KewneWhimsy <josshanel@gmail.com>
2026-05-18 23:42:50 +0200

    maj astro 6.5.3

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,6 @@
-// src/content/config.ts
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
 
 const projectsCollection = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/content/projects" }),
